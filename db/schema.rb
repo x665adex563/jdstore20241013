92de27e2ad19986e701096c83027f66e38255b5d
Knight~ <x665adex563@gmail.com>
2024-08-01 15:06:20 +0800

    完成 加入購物車按鈕

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20240729095424) do
+ActiveRecord::Schema.define(version: 20240801064454) do
+
+  create_table "product_lists", force: :cascade do |t|
+    t.integer  "order_id"
+    t.string   "product_name"
+    t.integer  "product_price"
+    t.integer  "quantity"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
