cdf545cc02be8089299536be4b8543a61b19fc1d
Knight~ <x665adex563@gmail.com>
2024-10-07 14:43:31 +0800

    修改錯字，在 app/controllers/application_controller.rb的第三行 helper_methos 改為 helper_method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_methos :current_cart
+  helper_method :current_cart
 
   def admin_required
     if !current_user.admin?
