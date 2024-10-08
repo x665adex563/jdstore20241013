ceb56a088d85b6e22bab18a3f698db21cdb5383e
Knight~ <x665adex563@gmail.com>
2024-10-08 14:25:54 +0800

    完成 讓訂單的網址號碼是亂數

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20241007074518) do
+ActiveRecord::Schema.define(version: 20241008061726) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20241007074518) do
     t.string   "shipping_address"
     t.datetime "created_at",                   null: false
     t.datetime "updated_at",                   null: false
+    t.string   "token"
   end
 
   create_table "product_lists", force: :cascade do |t|
